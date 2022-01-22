919949a5aa6e5335a2e494bb63efd27a3e980a5c
z to zee h <zhoomart.d@gmail.com>
2022-01-22 18:47:17 +0600

    fixed: timer

--- task-frame.js
+++ task-frame.js
@@ -1,33 +1,46 @@
-
 const modalWindow = document.querySelector('#modal-window');
-const openButton = document.querySelector('#open-button');
-const closeButton = document.querySelector('#close-button');
-const outside = document.querySelector('#outside');
 
+const openButton = document.querySelector('#open-button');
 openButton.onclick = function () {
     modalWindow.style.display = 'block';
 }
 
+const closeButton = document.querySelector('#close-button');
 closeButton.onclick = function () {
     modalWindow.style.display = 'none';
 }
 
+const outside = document.querySelector('#outside');
 window.onclick = function (event) {
-    if(event.target == outside) {
+    if (event.target == outside) {
         modalWindow.style.display = 'none';
     }
 }
 
-let sec = 28;
 
-function stopwatch() {
-    document.getElementById('second').textContent = sec--;
+//***** timer ******//
+let count = 27;
+let mss = 59;
+const sElem = document.querySelector('#s');
+const ms = document.querySelector('#ms');
+
+sElem.textContent = count;
+
+const startElem = document.querySelector('#btnStart');
+
+
+function countSequence() {
+    ms.textContent = mss--;
+    if (mss === 0) {
+        mss.textContent = '0' + mss;
+    }
 }
 
-function startCount (event) {
-    setInterval(stopwatch, 1000)
+function start () {
+    sElem.textContent = count--;
 }
 
-btnStart.onclick = function () {
-    startCount();
+startElem.onclick = function () {
+    setInterval(start, 1000);
+    setInterval(countSequence, 10);
 }
